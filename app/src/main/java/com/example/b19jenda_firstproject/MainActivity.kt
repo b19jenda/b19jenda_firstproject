f69074ddc55596864e399da791e9f87eb0dcebb6
b19jenda <b19jenda@student.his.se>
2020-03-31 22:25:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/b19jenda_firstproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.b19jenda_firstproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
